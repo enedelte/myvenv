7b2f0261901ea96d9ae2d451b378e97f4bc45afc
daalejoar@gmail.com <daalejoar@gmail.com>
2023-08-27 17:34:30 -0500

    -m "novedad1"

--- myvenv/scripts_python/databases/conecctionsDB.py
+++ myvenv/scripts_python/databases/conecctionsDB.py
@@ -1,4 +1,8 @@
 import pypyodbc as odbc
+import pandas  as pd 
+from credential import username, password
 
-DRIVER_NAME = ¨SQL SERVER¨
-SERVER_NAME = ""
+server = 'connectiosneps.database.windows.net'
+database = 'connectionpqr'
+connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:connectiosneps.database.windows.net,1433;Database=connectionpqr;Uid=CloudSA207c7bec;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+conn = odbc.connect(connection_string)
