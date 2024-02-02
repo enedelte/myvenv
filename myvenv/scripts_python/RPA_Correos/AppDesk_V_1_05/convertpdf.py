5b5e40a22b60c6acffdb658a3b03e104a93a4750
daalejoar@gmail.com <daalejoar@gmail.com>
2024-02-01 22:17:15 -0500

    - V.1.0.5

--- myvenv/scripts_python/RPA_Correos/AppDesk_V_1_05/convertpdf.py
+++ myvenv/scripts_python/RPA_Correos/AppDesk_V_1_05/convertpdf.py
@@ -9,7 +9,7 @@ def convertir_word_a_pdf(carpeta_origen, carpeta_destino):
     rutas_word = [os.path.join(carpeta_origen, archivo) for archivo in os.listdir(carpeta_origen) if archivo.lower().endswith('.docx')]
 
     if not rutas_word:
-        messagebox.showwarning("Advertencia", f"No se encontraron archivos DOCX en la carpeta de origen: {carpeta_origen}.")
+        messagebox.showwarning("Advertencia", f"No se encontraron archivos en la carpeta de origen: {carpeta_origen}.")
         return
 
     # Convertir los archivos DOCX a PDF en la carpeta de destino
@@ -32,14 +32,14 @@ def seleccionar_rutas():
     root.withdraw()  # Ocultar la ventana principal
 
     # Solicitar la carpeta de origen donde se encuentran los archivos DOCX
-    carpeta_origen = filedialog.askdirectory(title="Seleccionar carpeta de origen para archivos DOCX")
+    carpeta_origen = filedialog.askdirectory(title="Seleccionar carpeta de respuestas")
 
     if not carpeta_origen:
         messagebox.showwarning("Advertencia", "No se seleccionó la carpeta de origen.")
         return
 
     # Solicitar la carpeta de destino para guardar los archivos PDF
-    carpeta_destino = filedialog.askdirectory(title="Seleccionar carpeta de destino para PDF")
+    carpeta_destino = filedialog.askdirectory(title="Seleccionar carpeta de destino para respuestas en formato PDF")
 
     if not carpeta_destino:
         messagebox.showwarning("Advertencia", "No se seleccionó la carpeta de destino.")
@@ -50,7 +50,7 @@ def seleccionar_rutas():
         convertir_word_a_pdf(carpeta_origen, carpeta_destino)
     except Exception as e:
         messagebox.showerror("Error general", f"Error general: {e}")
-
+    
 if __name__ == "__main__":
     # Llamar a la función que permite seleccionar las rutas
     seleccionar_rutas()
--- myvenv/scripts_python/RPA_Correos/AppDesk_V_1_05/funciones.py
+++ myvenv/scripts_python/RPA_Correos/AppDesk_V_1_05/funciones.py
@@ -1,22 +1,14 @@
 import os
-import win32com.client as win32
-import pandas as pd
-from tkinter import filedialog, messagebox
+import csv
 import docx
 import re
-import csv
+import pandas as pd
+from tkinter import filedialog, messagebox
+import win32com.client as win32
 
-<<<<<<< HEAD:myvenv/scripts_python/RPA_Correos/AppDesk1/functions.py
-
-
-=======
-outlook = win32.Dispatch("Outlook.Application")
-ruta_base = r"C:\\Users\\" #cambiar dirección según tipo de usuario
->>>>>>> e2b4a1453cf4490108f7b7cb45f6a67830b1d864:myvenv/scripts_python/RPA_Correos/test/functions.py
 alias = "respuestas.pqr@nuevaeps.com.co"
 
 def enviar_correo_desde_alias(destinatario, asunto, cuerpo, adjunto, alias):
-    
     outlook = win32.Dispatch("Outlook.Application")
     mail = outlook.CreateItem(0)
     mail.SentOnBehalfOfName = alias
@@ -24,16 +16,11 @@ def enviar_correo_desde_alias(destinatario, asunto, cuerpo, adjunto, alias):
     mail.Subject = asunto
     mail.Body = cuerpo
 
-    ruta_pdf = filedialog.askdirectory(title="Selecciona la carpeta de adjuntos")
-    if not ruta_pdf:
-        messagebox.showwarning("Advertencia", "No se seleccionó la carpeta de adjuntos")
-        exit()
-
-    ruta_completa = os.path.join(ruta_pdf, str(adjunto))
-    if os.path.exists(ruta_completa):
-        mail.Attachments.Add(ruta_completa)
+    ruta_pdf = os.path.join(respuestas_path, str(adjunto))
+    if os.path.exists(ruta_pdf):
+        mail.Attachments.Add(ruta_pdf)
     else:
-        print(f"No se pudo encontrar el archivo: {ruta_completa}")
+        messagebox.showinfo('Advertebcia',f"No se pudo encontrar el archivo: {ruta_pdf}")
 
     mail.Send()
 
@@ -41,18 +28,15 @@ def enviar_correos_desde_csv(archivo_csv):
     df = pd.read_csv(archivo_csv)
 
     for index, fila in df.iterrows():
-        destinatario = fila['Correo Electronico']
+        destinatario = fila['Correos Electronicos']
         asunto = 'RESPUESTA SOLICITUD {}'.format(fila['Nombre Archivo'])
         cuerpo = 'Reciba un cordial saludo en nombre de NUEVA EPS S.A. Agradecemos su confianza al exponernos sus inquietudes.\n\n Adjunto remitimos respuesta a su Solicitud.\n\nPorque nos interesa ofrecerle un mejor servicio, queremos que nos cuente cómo fue su experiencia con la solución de su solicitud PQRS para lo cual lo invitamos a contestar dos preguntas en el siguiente enlace https://forms.office.com/r/pJaFmjkLW1\n\n“En el presente mensaje se tuvo en cuenta la protección de datos consagrada en la Ley 1581 DE 2012. Por favor no responda a este mensaje ya que la cuenta de correo se encuentra configurada sólo para generar respuestas masivas”.\n\n PRUEBA ELECTRÓNICA: Al recibir el acuse de recibo con destino a ésta oficina, se entenderá como aceptado y tendrá recepción como documento prueba de la entrega del usuario. (Ley 527 del 18/08/1999).\n\nAtentamente,\n\nDirección de Gestión Operativa\nVicepresidencia de Operaciones\n Bogotá D.C. – Colombia\n https://www.nuevaeps.com.co/.'.format(fila['Nombre Archivo'])
         adjunto = str(fila['Adjunto'])
 
         enviar_correo_desde_alias(destinatario, asunto, cuerpo, adjunto, alias)
 
-def seleccionar_archivo_csv():
-    archivo_csv = filedialog.askopenfilename(title="Seleccionar archivo CSV", filetypes=[("Archivos CSV", "*.csv")])
-    if archivo_csv:
-        enviar_correos_desde_csv(archivo_csv)
-        print('Se han enviado por completo los correos relacionados')
+    messagebox.showinfo("Completado", "Se han enviado los correos electrónicos de manera exitosa.")
+    os.remove(archivo_csv)
 
 def extract_emails_from_docx(docx_file):
     doc = docx.Document(docx_file)
@@ -66,16 +50,18 @@ def extract_emails_from_docx(docx_file):
     return emails
 
 def main():
-    folder_path = filedialog.askdirectory(title="Seleccionar carpeta de respuestas")
-    if not folder_path:
+    global respuestas_path  # Hacer la variable global para poder accederla desde otras funciones
+    respuestas_path = filedialog.askdirectory(title="Seleccionar carpeta de respuestas")
+    if not respuestas_path:
         messagebox.showwarning("Advertencia", "No se seleccionó la carpeta de documentos.")
         return
+
     # Lista para almacenar los correos electrónicos de todos los archivos
     all_emails = []
 
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(respuestas_path):
         if filename.endswith(".docx") and not filename.startswith('~$'):
-            docx_file_path = os.path.join(folder_path, filename)
+            docx_file_path = os.path.join(respuestas_path, filename)
             emails = extract_emails_from_docx(docx_file_path)
 
             if emails:
@@ -85,23 +71,20 @@ def main():
                 # Crear el nombre del archivo con extensión .pdf
                 nombre_archivo_pdf = f"{nombre_archivo_sin_extension}.pdf"
 
-                all_emails.append((nombre_archivo_sin_extension, emails, nombre_archivo_pdf))
+                all_emails.append((nombre_archivo_sin_extension, "; ".join(emails), nombre_archivo_pdf))
 
     # Escribir los correos electrónicos en un archivo CSV
-    csv_file_path = filedialog.asksaveasfilename(title="Guardar archivo CSV", defaultextension=".csv", filetypes=[("Archivos CSV", "*.csv")])
-    if not csv_file_path:
-        messagebox.showwarning("Advertencia", "No se seleccionó la ubicación para guardar el archivo CSV.")
-        return
-
+    csv_file_path = os.path.join(respuestas_path, "emails.csv")
     with open(csv_file_path, mode='w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Nombre Archivo", "Correo Electronico", "Adjunto"])
-        for nombre_archivo, emails, nombre_archivo_pdf in all_emails:
-            csv_writer.writerow([nombre_archivo, ';'.join(emails), nombre_archivo_pdf])
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        header_row = ["Nombre Archivo", "Correos Electronicos", "Adjunto"]
+        csv_writer.writerow(header_row)
 
-    messagebox.showinfo("Completado", f"Se han recolectado los correos electrónicos y se han guardado en '{csv_file_path}'.")
+        for nombre_archivo, emails, nombre_archivo_pdf in all_emails:
+            csv_writer.writerow([nombre_archivo, emails, nombre_archivo_pdf])
+
+    #messagebox.showinfo("Completado", f"Se han recolectado los correos electrónicos y se han guardado en '{csv_file_path}'.")
+    enviar_correos_desde_csv(csv_file_path)
 
 if __name__ == "__main__":
     main()
-
-
